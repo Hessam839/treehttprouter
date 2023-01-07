eec99b9fab6961a8da12771a4f8990f17cb5ce8c
Hessam839 <hessam.h.ir@gmail.com>
2023-01-07 22:04:50 +0330

    add some route methods

--- route.go
+++ route.go
@@ -14,6 +14,23 @@ func newRoute() *Route {
 	}
 }
 
-func (r *Route) add(method string, h *Handler) {
+func (r *Route) add(method string, h *Handler) *Route {
 	r.handler[method] = h
+	return r
+}
+
+func (r Route) get(h *Handler) {
+	r.add("GET", h)
+}
+
+func (r Route) post(h *Handler) {
+	r.add("POST", h)
+}
+
+func (r Route) put(h *Handler) {
+	r.add("PUT", h)
+}
+
+func (r Route) delete(h *Handler) {
+	r.add("DELETE", h)
 }
